34415454c1f0dee906972f0a596a0dac9cef7c2d
alinatelychko <adelin9999@gmail.com>
2023-04-11 22:30:03 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//task 1
+let firstNum = prompt("Enter your first month salary:", '1');
+let secondNum = prompt("Enter your second month salary:", '2');
+
+let firstSalary = +firstNum;
+let secondSalary = +secondNum;
+
+let result = firstSalary + secondSalary + 1;
+
+alert(`Your salary for 2 monthes is: ${result}$`);
